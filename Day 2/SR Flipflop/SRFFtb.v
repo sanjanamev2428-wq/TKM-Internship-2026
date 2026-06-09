25a34ed81677a8805acc48fa33c39377bdbb2667
SANJANA M <sanjanam.ev2428@saintgits.org>
2026-06-09 20:32:19 +0530

    Add files via upload

---
+++ Day 2/SR Flipflop/SRFFtb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/09/2026 07:34:44 PM
+// Design Name: 
+// Module Name: SRFFtb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SRFFtb(
+
+    );
+endmodule
