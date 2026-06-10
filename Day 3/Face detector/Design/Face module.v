4dd2462c828a806636e4e3e110276d6cfdec08fc
SANJANA M <sanjanam.ev2428@saintgits.org>
2026-06-10 21:52:30 +0530

    Update Face module.v

--- Day 3/Face detector/Design/Face module.v
+++ Day 3/Face detector/Design/Face module.v
@@ -1,1 +1,10 @@
-
+module face_mod(input clk,
+    input [7:0] S_in,
+    output reg [7:0] S_out,
+    output reg wr_en
+);
+    always @(posedge clk) begin
+        S_out <= S_in;
+        wr_en <= 1'b1;
+    end
+endmodule
