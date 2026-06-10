9b6008e8fb82dcee48e880cf4b08bd3395b1c7c9
SANJANA M <sanjanam.ev2428@saintgits.org>
2026-06-10 20:01:26 +0530

    Create Sequence detector.v

---
+++ Day 3/Sequence detector/Design/Sequence detector.v
@@ -0,0 +1,84 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10.06.2026 13:33:35
+// Design Name: 
+// Module Name: detector1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module detector1(
+input clk ,
+input reset,din,
+  output reg detected
+  );
+  parameter idle=2'b00;
+  parameter s1=2'b01;
+  parameter s2=2'b10;
+  parameter s3=2'b11;
+  reg [1:0] ps, ns;
+ 
+ always @(posedge clk)
+    begin
+      if(reset)
+        begin
+          ps<=idle;
+        end
+      else
+        ps<=ns;
+    end
+     
+  always@(*)
+    begin
+      case(ps)
+        idle: begin
+          detected=0;
+          if(din==0)
+            begin
+              ns=idle;
+            end
+          else
+            ns=s1;
+        end
+       
+        s1:begin
+          if(din==0)
+            begin
+              ns=idle;
+            end
+          else
+            ns=s2;
+        end
+       
+        s2:begin
+          if(din==0)
+            ns=idle;
+          else
+            ns=s3;
+        end
+       
+        s3:begin
+          if(din==0)
+            begin
+              ns=idle;
+              detected=1;
+            end
+          else
+            ns=s3;
+        end
+      endcase
+    end
+endmodule
