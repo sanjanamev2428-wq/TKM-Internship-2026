a35fa672f05c18923a95493101f70eeaf0f17b06
SANJANA M <sanjanam.ev2428@saintgits.org>
2026-06-10 21:56:05 +0530

    Update Face detector_Tb.v

--- Day 3/Face detector/Tb/Face detector_Tb.v
+++ Day 3/Face detector/Tb/Face detector_Tb.v
@@ -1,1 +1,38 @@
+module top_module_tb;
+reg clk;
+    reg rst;
+    reg [7:0] S_in;
+    wire [7:0] final_out;
 
+    top_module uut (
+        .clk(clk),
+        .rst(rst),
+        .S_in(S_in),
+        .final_out(final_out)
+    );
+
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk;
+    end
+
+    initial begin
+        rst = 1;
+        S_in = 0;
+        #20;
+        
+        rst = 0;
+        #10;
+        
+        S_in = 8'hD1; #10;
+        S_in = 8'hD2; #10;
+        S_in = 8'hD3; #10;
+        S_in = 8'hD4; #10;
+        S_in = 8'hD5; #10;
+        
+        S_in = 8'h00;
+        
+        #300;
+        $finish;
+    end
+endmodule
